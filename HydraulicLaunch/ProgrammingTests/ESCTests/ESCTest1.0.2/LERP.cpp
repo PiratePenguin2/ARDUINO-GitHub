c02fe848bac82f4b1b29ac5e329f7a911c600926
PiratePenguin2 <PiratePenguin2.2649@gmail.com>
2024-03-02 15:33:19 -0800

    Got rid of 1.0.2, moving it instead to a new catagory of 2.0.0

--- HydraulicLaunch/ProgrammingTests/ESCTests/ESCTest1.0.2/LERP.cpp
+++
@@ -1,6 +0,0 @@
-// LERP.cpp
-#include "LERP.h"
-
-float lerp(float a, float b, float weight) {
-    return a * (1 - weight) + b * weight;
-}
--- HydraulicLaunch/ProgrammingTests/ESCTests/ESCTest1.0.2/LERP.h
+++
@@ -1,7 +0,0 @@
-// LERP.h
-#ifndef LERP_h
-#define LERP_h
-
-float lerp(float a, float b, float weight);
-
-#endif
