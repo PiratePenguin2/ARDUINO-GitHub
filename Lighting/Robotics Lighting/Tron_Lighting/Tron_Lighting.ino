2996fcc67d550073fb3fdb88fd48f7f4a3bdd9e7
Luke B. <PiratePenguin2.2649@gmail.com>
2025-06-09 08:58:00 -0700

    TronLighting Pin Switch

--- Lighting/Robotics Lighting/Tron_Lighting/Tron_Lighting.ino
+++ Lighting/Robotics Lighting/Tron_Lighting/Tron_Lighting.ino
@@ -1,6 +1,6 @@
 #include <FastLED.h>
 
-#define LED_PIN        2     // Data pin for LED strip
+#define LED_PIN        7     // Data pin for LED strip
 #define START_LED      2//113
 #define NUM_LEDS       86//61    // Number of LEDs
 #define WAVE_SPEED     3     // Speed of the moving wave
